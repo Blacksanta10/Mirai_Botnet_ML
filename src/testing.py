d6d405ebf7fd1ddab74ddb060456b8241bb865ba
Demitrius Webb <moutonwebb29@gmail.com>
2026-03-25 22:05:16 -0400

    Added more evaluations to test after running model prdictions. Changed name of knn method in train.py"

--- src/testing.py
+++ src/testing.py
@@ -1,1 +1,13 @@
-# Step 3: Purpose is to test model prediction 
+""" Step 3: Purpose is to create model predictions,
+then to show different metrics based on predictions 
+""" 
+
+from sklearn.metrics import classification_report, accuracy_score
+
+def evaluate(model, X_test, y_test):
+    predictions = model.predict(X_test)
+
+    
+    print("Accuracy:", accuracy_score(y_test, predictions))
+    print("\nClassififcation Report:")
+    print(classification_report(y_test, predictions))   #text report
--- src/train.py
+++ src/train.py
@@ -3,8 +3,8 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 # Example for Nearest Neighbors Classification method
-def train_kNN(X_train, y_train, n_neighbors=5):
-    model = KNeighborsClassifier(n_neighbors=n_neighbors)
-    model.fit(X_train, y_train)
+def train_knn(X_train, y_train, k=5):
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(X_train, y_train)     #needs to be fit before predicting
     return model
 
